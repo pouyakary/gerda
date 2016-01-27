a60369bce49599ae4035318898b959690e6173d1
Pouya Kary <k@karyfoundation.org>
2016-01-27 19:52:18 +0330

    playground main updated

--- playground/main.js
+++ playground/main.js
@@ -1,50 +1,57 @@
-'use strict';
+//
+// Gerda -  The optimized Arendelle's intelligent auto suggestion server 
+//    Copyright 2016 - Pouya Kary <k@arendelle.org>
+//
 
-const electron = require('electron');
-// Module to control application life.
-const app = electron.app;
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow;
+/* ────────────────────────────────────────────────────────────────────────────────────────── *
+ * ::::::::::::::::::::::::::::: G E R D A   P L A Y G R O U N D :::::::::::::::::::::::::::: *
+ * ────────────────────────────────────────────────────────────────────────────────────────── */
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
+//
+// ─── HEADER ─────────────────────────────────────────────────────────────────────
+//
 
-function createWindow () {
-	// Create the browser window.
-	mainWindow = new BrowserWindow (
-		{ width: 800 , height: 400 }
-	);
+	'use strict';
 
-  // and load the index.html of the app.
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
+	const electron = require( 'electron' );
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
-}
+	const app = electron.app;
+	const BrowserWindow = electron.BrowserWindow;
+	let mainWindow;
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-app.on('ready', createWindow);
+//
+// ─── DISPLAY ────────────────────────────────────────────────────────────────────
+//
 
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
+	function createWindow () {
+		mainWindow = new BrowserWindow ({
+			width: 800 , height: 400 
+		});
+		mainWindow.loadURL( 'file://' + __dirname + '/index.html' );
+		mainWindow.on( 'closed' , function() {
+			mainWindow = null;
+		});
+	}
 
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow();
-  }
-});
+//
+// ─── MAIN ───────────────────────────────────────────────────────────────────────
+//
+
+	app.on( 'ready' , createWindow );
+
+//
+// ─── DISPLAY HANDLERS ───────────────────────────────────────────────────────────
+//
+
+	app.on( 'window-all-closed' , function () {
+		console.log( 'Goodbye' );
+		app.quit();
+	});
+	
+	app.on( 'activate', function () {
+		if (mainWindow === null) {
+		createWindow();
+		}
+	});
+
+// ────────────────────────────────────────────────────────────────────────────────
