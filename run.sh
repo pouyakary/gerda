3cc0080bd9cb7bfdee46c37db58df0ac386b07cf
Pouya Kary <k@karyfoundation.org>
2016-01-31 13:51:47 +0330

    cleaned up

--- run.sh
+++ run.sh
@@ -1,5 +1,19 @@
+#! /bin/bash
+#
+# Gerda - The optimized Arendelle itelegent auto suggestion's server
+#    Copyright 2016 Kary Foundation, Inc.
+#    Author: Pouya Kary <k@karyfoundation.org>
+#
+
+# Compiling Gerda
 tsc
+
+# optimizing Gerda
 cd playground/scripts/
 uglifyjs gerda-compiled.js --output gerda.js --mangle
+
+# Running the Electron Playground
 npm install && npm start
+
+# Done
 cd ../..
