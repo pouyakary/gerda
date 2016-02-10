632076c9765419307577072f0de14300fc574f6e
Pouya Kary <k@karyfoundation.org>
2016-02-10 12:37:37 +0330

    a change in plans

--- server/main.ts
+++ server/main.ts
@@ -7,3 +7,14 @@
  * :::::::::::::::::::::::::::::::::::::::: G E R D A ::::::::::::::::::::::::::::::::::::::: *
  * ────────────────────────────────────────────────────────────────────────────────────────── */
 
+/// <reference path="arendelle-tools/file-system/directory.ts" />
+/// <reference path="kernel/space-finder.ts" />
+
+module Gerda {
+	export function Suggest ( fileSystem: Arendelle.Directory , currentFile: Arendelle.File , caretLocation: number , trigger: string ): Array<string> {
+
+		
+
+		return [ ];
+	}
+}
